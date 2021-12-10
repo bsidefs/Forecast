12c73d5676164430156cfc284cfbade009dc8ead
xbtamsing <54383855+xbtamsing@users.noreply.github.com>
2021-12-09 19:22:59 -0800

    Initial Commit

---
+++ Forecast/Supporting Files/ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastApp.swift
+//  Forecast
+//
+//  Created by Brian Tamsing on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
